00db0b229976f18c530f15ea5c358f813466b557
ngthgph <ngthgph112@gmail.com>
2023-11-05 18:58:27 +0700

    Relocate app logic and Fix some error in ViewModel

--- app/src/main/java/com/example/dessertclicker/data/DessertUiState.kt
+++ app/src/main/java/com/example/dessertclicker/data/DessertUiState.kt
@@ -5,7 +5,7 @@ import com.example.dessertclicker.data.Datasource.dessertList
 
 data class DessertUiState(
     val revenue: Int = 0,
-    val dessertSold: Int = 0,
+    val dessertsSold: Int = 0,
     val currentDessertIndex: Int = 0,
     val currentDessertPrice: Int = dessertList[currentDessertIndex].price,
     @DrawableRes val currentDessertImageId: Int = dessertList[currentDessertIndex].imageId
--- app/src/main/java/com/example/dessertclicker/ui/DessertViewModel.kt
+++ app/src/main/java/com/example/dessertclicker/ui/DessertViewModel.kt
@@ -1,12 +1,50 @@
 package com.example.dessertclicker.ui
 
+import androidx.lifecycle.ViewModel
+import com.example.dessertclicker.data.Datasource.dessertList
+import com.example.dessertclicker.data.DessertUiState
+import com.example.dessertclicker.model.Dessert
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
-class DessertViewModel {
-    private val _dessertUiState = MutableStateFlow(DessertViewModel())
-    val dessertViewModel: StateFlow<DessertViewModel> = _dessertUiState.asStateFlow()
+class DessertViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(DessertUiState())
+    val uiState: StateFlow<DessertUiState> = _uiState.asStateFlow()
 
+    fun updateImageOnClick() {
+        _uiState.update{
+            val dessertToShow = determineDessertToShow(dessertList, it.dessertsSold)
+            it.copy(
+                revenue = it.revenue + it.currentDessertPrice,
+                dessertsSold = it.dessertsSold + 1,
+                currentDessertImageId = dessertToShow.imageId,
+                currentDessertPrice = dessertToShow.price
+            )
+        }
+    }
 
+    /**
+     * Determine which dessert to show.
+     */
+    private fun determineDessertToShow(
+        desserts: List<Dessert>,
+        dessertsSold: Int
+    ): Dessert {
+        var dessertToShow = desserts.first()
+        for (dessert in desserts) {
+            if (dessertsSold >= dessert.startProductionAmount) {
+                dessertToShow = dessert
+            } else {
+                // The list of desserts is sorted by startProductionAmount. As you sell more desserts,
+                // you'll start producing more expensive desserts as determined by startProductionAmount
+                // We know to break as soon as we see a dessert who's "startProductionAmount" is greater
+                // than the amount sold.
+                break
+            }
+        }
+
+        return dessertToShow
+    }
 }
